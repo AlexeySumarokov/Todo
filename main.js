ffe80998b913251a96dc592bf57af598308b629a
a.pahomov <a.pahomov@dunice.net>
2022-04-21 17:14:10 +0300

    add render

--- main.js
+++ main.js
@@ -2,12 +2,27 @@ const button = document.querySelector('.btn-primary')
 console.log(button);
 
 const input = document.querySelector('.form-control')
+let tasks = [];
 
+const textInput = document.querySelector('.todoList')
 console.log(input);
 
+const render = ()=>{
+    textInput.innerHTML = '';
+    tasks.forEach(element => {
+        textInput.innerHTML +=
+        `<li><input type="checkbox" id="${element.id}">
+        <span id="${element.id}">${element.text}</span>
+        <button id="${element.id}">X</button></li>`
+    });    
+}
+
 const getInputValue = () => {
-    console.log(input.value);
+    const newTodo = {id: Date.now(), text: input.value, status: false};
+    tasks.push(newTodo);
+    console.log(tasks);
+    render();
 }
 
 
-button.addEventListener('click', getInputValue)
+button.addEventListener('click', getInputValue) 
